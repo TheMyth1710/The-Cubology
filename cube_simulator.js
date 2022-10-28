adf271beb44039273e05cf8fe13c4556654c6e5b
The Myth <dagamudith@gmail.com>
2022-10-29 03:07:58 +0530

    Updated for Front Moves

--- cube_simulator.js
+++ cube_simulator.js
@@ -3,17 +3,17 @@ let cube = [
     [
         ['W', 'W', 'W'],
         ['W', 'W', 'W'],
-        ['W', 'W', 'W']
+        ['W', 'W', 'W'] // Top
     ],   
     [
         ['G', 'G', 'G'],
         ['G', 'G', 'G'],
-        ['G', 'G', 'G']
+        ['G', 'G', 'G'] // Front
     ],
     [
         ['R', 'R', 'R'],
         ['R', 'R', 'R'],
-        ['R', 'R', 'R']
+        ['R', 'R', 'R'] // Side
     ],
     [
         ['B', 'B', 'B'],
@@ -31,6 +31,7 @@ let cube = [
         ['Y', 'Y', 'Y']
     ],
 ]
+
 /*
 
 Layers:
@@ -44,7 +45,6 @@ document.querySelectorAll('.cube-notation').forEach(move => move.addEventListene
     cube_notation(move);
 }))
 
-
 function cube_notation(move){
     const notation = move.innerHTML;
     if (['U','U\'','D','D\''].includes(notation)){
@@ -54,28 +54,65 @@ function cube_notation(move){
         const cube_g = structuredClone(cube[1]);
         if (['D','U\''].includes(notation)) direction = -1;
         if (['D','D\''].includes(notation)) layer = 2;
-        for (let i = 0; i < lateral_cube.length; i++){
+        for (let i = 0, n = lateral_cube.length; i < n; i++){
             if (i+direction == 4) cube[4][layer] = cube_g[layer];
             else cube[i+1][layer] = lateral_cube.at(i+direction)[layer];
         }
     }else if (['R','R\'','L','L\''].includes(notation)){
         let layer = 2;
-        let direction = 1;
+        let direction = -1;
         let front_cube = structuredClone([cube[1],cube[0],cube[3],cube[5]]);
         let front_cube_clone = structuredClone(front_cube);
-        const cube_g = structuredClone(front_cube[0]);
-        if (['R','L\''].includes(notation)) direction = -1;
+        if (['R\'','L'].includes(notation)) direction = 1;
         if (['L','L\''].includes(notation)) layer = 0;
-        for (let i = 0; i < front_cube.length; i++){
+        for (let i = 0, n = front_cube.length; i < n; i++){
             for (let j = 0; j < 3; j++){
-                if (i+direction == 4) front_cube[i][j][layer] = cube_g[j][layer];
+                if (i == 2) front_cube[i][j][Math.abs(layer-2)] = front_cube_clone.at(i+direction)[j][layer]; // To inverse the B
+                else if (i+direction == 4) front_cube[i][j][layer] = front_cube_clone[0][j][layer];
+                else if (i == 3 || i == direction) front_cube[i][j][layer] = front_cube_clone.at(i+direction)[j][Math.abs(layer-2)] // To inverse of B on Y
                 else front_cube[i][j][layer] = front_cube_clone.at(i+direction)[j][layer];
             }
         }
-        console.log(front_cube);
+        console.log(front_cube)
         for (const[i,j] of Object.entries({0:1,1:0,2:3,3:5})){
             cube[j] = front_cube[i];
         }
-    }
+    }else if (['F','F\''].includes(notation)){
+        let dynamic_cube = structuredClone([cube[0],cube[2],cube[5],cube[4]]);
+        let dynamic_cube_copy = structuredClone(dynamic_cube);
+        let direction = -1;
+        if (notation == 'F\'') direction = 1;
+        for (let j = 0; j < 3; j++){
+            dynamic_cube[0][2][j] = dynamic_cube_copy.at(0+direction)[j][2];
+            dynamic_cube[1][j][0] = dynamic_cube_copy[1+direction][2][j];
+            dynamic_cube[2][0][j] = dynamic_cube_copy[2+direction][j][0];
+            if (3 + direction == 4) dynamic_cube[3][j][2] = dynamic_cube_copy[0][0][j]
+            else dynamic_cube[3][j][2] = dynamic_cube_copy[3+direction][0][j];
+        }
+        for (const[i,j] of Object.entries({0:0,1:2,2:5,3:4})){
+            cube[j] = front_cube[i];
+        }
+    }//For B B'
+    // B' - Y O W R
+    // W R Y O
+    /*
+
+    W   W   W
+    W   W   W
+    O   O   O
+
+    W   R   R
+    W   R   R
+    W   R   R
+
+    R   R   R
+    Y   Y   Y
+    Y   Y   Y
+
+    O   O   Y
+    O   O   Y
+    O   O   Y
+
+    */
     console.log(cube);
 }
