cc015fd3b0dbaeabc5247f17c5c0296939b36fdf
Mudith Daga <dagamudith@gmail.com>
2022-11-06 20:21:51 +0530

    No Source Code Revealing

--- main.js
+++ main.js
@@ -108,3 +108,22 @@ document.onscroll = function(){
     }
     lastScrollTop = st <= 0 ? 0 : st;
 }
+
+document.onkeydown = function(e) {
+    if(e.keyCode == 123) {
+     return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+     return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+     return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+     return false;
+    }
+
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)){
+     return false;
+    }      
+ }
