4d8b1a7b7437c269629ccdd304d067fdafd215d2
The Myth <dagamudith@gmail.com>
2022-11-14 02:24:38 +0530

    Updated Files

--- app/functions.py
+++ app/functions.py
@@ -46,16 +46,19 @@ def scrambleToCube(scramble, cube_state=None):
     for move in scramble:
         def recursive(move, cube_state, count=1):
             cube = deepcopy(cube_state)
-            if any(move == n for n in ["R", "L'"]):
+            repeat = False
+            if "2" in move: repeat = True; move = move.replace("2","")
+            if move in ["R", "L'"]:
                 l = 2; d = 2; r = 1;
                 if move == "L'": l = 0; d = 4; r = -1
                 for j in range(3):
-                    cube[0][l][j] = cube_state[1][j][l]
-                    cube[1][j][l] = cube_state[5][abs(l-2)][abs(j-2)]
-                    cube[5][abs(l-2)][j] = cube_state[3][j][abs(l-2)]
-                    cube[3][j][abs(l-2)] = cube_state[0][l][abs(j-2)]
+                    cube[0][l][j] = cube_state[1][abs(j-2)][l]
+                    cube[1][j][l] = cube_state[5][abs(l-2)][j]
+                    cube[5][abs(l-2)][j] = cube_state[3][abs(j-2)][abs(l-2)]
+                    cube[3][j][abs(l-2)] = cube_state[0][l][j]
+                # print(cube,'\n')
                 cube[d] = rotateMatrix(cube[d], r)
-            elif any(move == n for n in ["R'", "L"]):
+            elif move in ["R'", "L"]:
                 l = 2; d = 2; r = -1
                 if move == "L": l = 0; d = 4; r = 1
                 for j in range(3):
@@ -64,16 +67,16 @@ def scrambleToCube(scramble, cube_state=None):
                     cube[5][abs(l-2)][j] = cube_state[1][j][l]
                     cube[3][j][abs(l-2)] = cube_state[5][abs(l-2)][abs(j-2)]
                 cube[d] = rotateMatrix(cube[d], r)
-            elif any(move == n for n in ["U", "D'"]):
+            elif move in ["U", "D'"]:
                 l = 2; d = 5; r = 1
                 if move == "D'": l = 0; d = 0; r = -1
                 for j in range(3):
                     cube[1][l][j] = cube_state[4][l][j]
-                    cube[2][l][j] = cube_state[1][l][j]
-                    cube[3][l][j] = cube_state[2][l][j]
                     cube[4][l][j] = cube_state[3][l][j]
+                    cube[3][l][j] = cube_state[2][l][j]
+                    cube[2][l][j] = cube_state[1][l][j]
                 cube[d] = rotateMatrix(cube[d], r)
-            elif any(move == n for n in ["U'", "D"]):
+            elif move in ["U'", "D"]:
                 l = 2; d = 5; r = -1
                 if move == "D": l = 0; d = 0; r = 1
                 for j in range(3):
@@ -82,21 +85,36 @@ def scrambleToCube(scramble, cube_state=None):
                     cube[3][l][j] = cube_state[4][l][j]
                     cube[4][l][j] = cube_state[1][l][j]
                 cube[d] = rotateMatrix(cube[d], r)
-            if "2" in move and count == 1:
+            elif move in ["F", "B'"]:
+                l = 2; d = 3; r = 1
+                if move == "B'": l = 0; d = 1; r = -1
+                for j in range(3):
+                    cube[0][j][l] = cube_state[2][j][l]
+                    cube[2][j][l] = cube_state[5][j][l]
+                    cube[5][j][l] = cube_state[4][abs(j-2)][abs(l-2)]
+                    cube[4][j][abs(l-2)] = cube_state[0][abs(j-2)][l]
+                cube[d] = rotateMatrix(cube[d], r)
+            elif move in ["F'", "B"]:
+                l = 2; d = 3; r = -1
+                if move == "B": l = 0; d = 1; r = 1
+                for j in range(3):
+                    cube[0][j][l] = cube_state[4][abs(j-2)][abs(l-2)]
+                    cube[4][j][abs(l-2)] = cube_state[5][abs(j-2)][l]
+                    cube[5][j][l] = cube_state[2][j][l]
+                    cube[2][j][l] = cube_state[0][j][l]
+                cube[d] = rotateMatrix(cube[d], r)
+            if repeat and count == 1:
                 cube = recursive(move, cube, 2)
             return cube
-        cube = recursive(move, cube)
-    return cube
-        # return ''.join(listToString(cube))
-print(scrambleToCube(["R", "U", "R'", "U'"]))
-
+        cube = recursive(move, cube, 1)
+    # return cube
+    return ''.join(listToString(cube))
+print(scrambleToCube(["R2", "F2", "U2", "D2", "L2", "B2"]))
 
 from rubik_solver import utils
-cube = 'yyyyyyyyybbbbbbbbbrrrrrrrrrgggggggggooooooooowwwwwwwww' # og
-cube = 'obgoywoyybbgybbywrwrbyrrwrgrgobgwwgoywwrooyyrbgrowoggb' # scrmable
+cube = 'wyyyyywyybgggbbbggroorroroobbgggbbbgorroororrywwwwwyww' # scrmable
 # print(utils.solve(cube, 'Beginner'))
 # print(utils.solve(cube, 'CFOP'))
-# print(utils.solve(cube, 'Kociemba'))
+print(utils.solve(cube, 'Kociemba')) # replace kociemba alg with wgr alg
 
-# add support for scramble to cubestring
 # add support to replace U, U to U2 and U, U, U, U to nothing for CFOP and beginner
