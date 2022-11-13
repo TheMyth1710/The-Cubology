8afd7d0270ed75cb3912969822408da074454c81
The Myth <dagamudith@gmail.com>
2022-11-13 22:54:42 +0530

    Updated Files

---
+++ app/functions.py
@@ -0,0 +1,12 @@
+def reverse_move(scramble):
+    return [move+"'" if all(m not in ["2","'"] for m in move) else move.replace("'","")  for move in scramble]
+
+from rubik_solver import utils
+cube = 'yyyyyyyyybbbbbbbbbrrrrrrrrrgggggggggooooooooowwwwwwwww' # og
+cube = 'obgoywoyybbgybbywrwrbyrrwrgrgobgwwgoywwrooyyrbgrowoggb' # scrmable
+print(utils.solve(cube, 'Beginner'))
+print(utils.solve(cube, 'CFOP'))
+print(utils.solve(cube, 'Kociemba'))
+
+# add support for scramble to cubestring
+# add support to replace U, U to U2 and U, U, U, U to nothing for CFOP and beginner
