b8ef4c57ec4e9d3521328a7fff484851b14cef58
The Myth <dagamudith@gmail.com>
2022-11-13 22:56:49 +0530

    Update functions.py

--- app/functions.py
+++ app/functions.py
@@ -1,6 +1,10 @@
 def reverse_move(scramble):
     return [move+"'" if all(m not in ["2","'"] for m in move) else move.replace("'","")  for move in scramble]
 
+def scrambleToCube(scramble, cube_state=None):
+    if cube_state: # convert cubestring to 3-indexed-array
+        pass
+
 from rubik_solver import utils
 cube = 'yyyyyyyyybbbbbbbbbrrrrrrrrrgggggggggooooooooowwwwwwwww' # og
 cube = 'obgoywoyybbgybbywrwrbyrrwrgrgobgwwgoywwrooyyrbgrowoggb' # scrmable
